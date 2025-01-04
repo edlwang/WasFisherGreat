ffabeb066fade39315385de4c3bd794f6ce1c319
Edward Wang <ewang43@jhu.edu>
2025-01-03 19:44:52 -0500

    Save scree plots

--- scree_plots.py
+++ scree_plots.py
@@ -5,5 +5,16 @@ full_prompt_llama = np.load('1-embeddings-random-full-prompt-llama-100.npy')
 full_prompt_nomic = np.load('1-embeddings-random-full-prompt-nomic-100.npy')
 
 llama_pca = PCA().fit(full_prompt_llama)
-print(llama_pca.explained_variance_)
-print(len(llama_pca.explained_variance_))
+plt.plot(range(1,551), llama_pca.explained_variance_)
+plt.title('Scree Plot for PCA on Llama Full Prompt Embeddings')
+plt.xlabel('Dimension')
+plt.ylabel('Explained Variance')
+plt.savefig('llama-scree')
+
+
+nomic_pca = PCA().fit(full_prompt_nomic)
+plt.title('Scree Plot for PCA on Nomic Full Prompt Embeddings')
+plt.plot(range(1,551), llama_pca.explained_variance_)
+plt.xlabel('Dimension')
+plt.ylabel('Explained Variance')
+plt.savefig('nomic-scree')
